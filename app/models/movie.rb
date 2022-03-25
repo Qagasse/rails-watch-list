5983f628aca797800fc25da8897e186e038762da
quentin agasse <quentin.agasse@gmail.com>
2022-03-25 16:20:10 +0100

    modif front

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   validates :title, :overview, uniqueness: true, presence: true 
   has_many :lists, through: :bookmarks
   has_many :bookmarks
+  has_one_attached :photo
 end
